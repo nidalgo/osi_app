048925d8b0f9333524413f40c57fa8b167d28d2d
Nicole Dalgo Ruales <nicole@nidalgo.nicolita>
2020-09-01 21:47:23 -0500

    tutorial de PhotoGallery Entre las funcionalidades estan tomar fotos y visualizar fotos capturadas

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,23 +1,171 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+import { camera, trash, close } from 'ionicons/icons';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
+         IonFab, IonFabButton, IonIcon, IonGrid, IonRow,
+         IonCol, IonImg, IonActionSheet } from '@ionic/react';
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
+
+export const GridExample: React.FC = () => (
+  <IonContent>
+    <IonGrid>
+      <IonRow>
+        <IonCol>ion-col</IonCol>
+        <IonCol>ion-col</IonCol>
+        <IonCol>ion-col</IonCol>
+        <IonCol>ion-col</IonCol>
+      </IonRow>
+
+      <IonRow>
+        <IonCol size="6">ion-col size="6"</IonCol>
+        <IonCol>ion-col</IonCol>
+        <IonCol>ion-col</IonCol>
+      </IonRow>
+
+      <IonRow>
+        <IonCol size="3">ion-col size="3"</IonCol>
+        <IonCol>ion-col</IonCol>
+        <IonCol size="3">ion-col size="3"</IonCol>
+      </IonRow>
+
+      <IonRow>
+        <IonCol size="3">ion-col size="3"</IonCol>
+        <IonCol size="3" offset="3">
+          ion-col size="3" offset="3"
+        </IonCol>
+      </IonRow>
+
+      <IonRow>
+        <IonCol>ion-col</IonCol>
+        <IonCol>
+          ion-col
+          <br />#
+        </IonCol>
+        <IonCol>
+          ion-col
+          <br />#
+          <br />#
+        </IonCol>
+        <IonCol>
+          ion-col
+          <br />#
+          <br />#
+          <br />#
+        </IonCol>
+      </IonRow>
+
+      <IonRow>
+        <IonCol className="ion-align-self-start">ion-col start</IonCol>
+        <IonCol className="ion-align-self-center">ion-col center</IonCol>
+        <IonCol className="ion-align-self-end">ion-col end</IonCol>
+        <IonCol>
+          ion-col
+          <br />#
+          <br />#
+        </IonCol>
+      </IonRow>
+
+      <IonRow className="ion-align-items-start">
+        <IonCol>start ion-col</IonCol>
+        <IonCol>start ion-col</IonCol>
+        <IonCol className="ion-align-self-end">start ion-col end</IonCol>
+        <IonCol>
+          ion-col
+          <br />#
+          <br />#
+        </IonCol>
+      </IonRow>
+
+      <IonRow className="ion-align-items-center">
+        <IonCol>center ion-col</IonCol>
+        <IonCol>center ion-col</IonCol>
+        <IonCol>center ion-col</IonCol>
+        <IonCol>
+          ion-col
+          <br />#
+          <br />#
+        </IonCol>
+      </IonRow>
+
+      <IonRow className="ion-align-items-end">
+        <IonCol>end ion-col</IonCol>
+        <IonCol className="ion-align-self-start">end ion-col start</IonCol>
+        <IonCol>end ion-col</IonCol>
+        <IonCol>
+          ion-col
+          <br />#
+          <br />#
+        </IonCol>
+      </IonRow>
+
+      <IonRow>
+        <IonCol size="12" size-sm>
+          ion-col size="12" size-sm
+        </IonCol>
+        <IonCol size="12" size-sm>
+          ion-col size="12" size-sm
+        </IonCol>
+        <IonCol size="12" size-sm>
+          ion-col size="12" size-sm
+        </IonCol>
+        <IonCol size="12" size-sm>
+          ion-col size="12" size-sm
+        </IonCol>
+      </IonRow>
+
+      <IonRow>
+        <IonCol size="12" size-md>
+          ion-col size="12" size-md
+        </IonCol>
+        <IonCol size="12" size-md>
+          ion-col size="12" size-md
+        </IonCol>
+        <IonCol size="12" size-md>
+          ion-col size="12" size-md
+        </IonCol>
+        <IonCol size="12" size-md>
+          ion-col size="12" size-md
+        </IonCol>
+      </IonRow>
+
+      <IonRow>
+        <IonCol size="6" size-lg offset="3">
+          ion-col size="6" size-lg offset="3"
+        </IonCol>
+        <IonCol size="3" size-lg>
+          ion-col size="3" size-lg
+        </IonCol>
+      </IonRow>
+    </IonGrid>
+  </IonContent>
+);
 
 const Tab2: React.FC = () => {
+  const {photos, takePhoto } = usePhotoGallery();
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Photo Gallery</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={index}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={() => takePhoto()}>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
       </IonContent>
     </IonPage>
   );
